3beeebce4e8e970418f156113cf949670599ff33
SameekshaIrkal <54880514+SameekshaIrkal@users.noreply.github.com>
2019-09-06 08:47:17 +0530

    Version1.1

--- Maximum.cpp
+++ Maximum.cpp
@@ -1,13 +1,6 @@
-//Maximum element in list
 #include <iostream>
-using namespace std;
-class Maximum
-{
-public:
-	void max(int[], int);
-};
-
-void Maximum::max(int list[], int size)
+#include "Maximum.h"
+int Maximum::max(int *list, int size)
 {
 	int big, index;
 	big = list[0];
@@ -16,23 +9,7 @@ void Maximum::max(int list[], int size)
 		if (list[index] > big)
 		{
 			big = list[index];
-		
 		}
 	}
-	cout << "Maximum element is : " << big;
-}
-
-int main()
-{
-	int size, element[5];
-	Maximum m1;
-	cout << "enter the size of array";
-	cin >> size;
-	cout << "enter array elements";
-	for (int start = 0; start < size; start++)
-	{
-		cin >> element[start];
-	}
-	m1.max(element, size);
-	return 0;
+	return big;
 }
